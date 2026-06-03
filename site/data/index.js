4a22e6899b020ec2fda8fc36cfa6be9f68b3abe7
eedede888-lab <eedede888@gmail.com>
2026-06-03 20:30:00 +0800

    feat: new 20260603

--- site/data/index.js
+++ site/data/index.js
@@ -1,8 +1,8 @@
 window.__DATAREG&&window.__DATAREG("index",{
   "dates": [
     {
-      "date": "20260602",
-      "label": "2026-06-02",
+      "date": "20260603",
+      "label": "2026-06-03",
       "has_market": true,
       "stocks": [
         {
@@ -18,7 +18,7 @@ window.__DATAREG&&window.__DATAREG("index",{
         {
           "code": "3105",
           "name": "穩懋",
-          "mkt": "TWO"
+          "mkt": "TW"
         },
         {
           "code": "6257",
@@ -28,135 +28,7 @@ window.__DATAREG&&window.__DATAREG("index",{
         {
           "code": "6261",
           "name": "久元",
-          "mkt": "TWO"
-        }
-      ]
-    },
-    {
-      "date": "20260601",
-      "label": "2026-06-01",
-      "has_market": true,
-      "stocks": [
-        {
-          "code": "2344",
-          "name": "華邦電",
           "mkt": "TW"
-        },
-        {
-          "code": "3037",
-          "name": "欣興",
-          "mkt": "TW"
-        },
-        {
-          "code": "3105",
-          "name": "穩懋",
-          "mkt": "TWO"
-        },
-        {
-          "code": "6257",
-          "name": "矽格",
-          "mkt": "TW"
-        },
-        {
-          "code": "6261",
-          "name": "久元",
-          "mkt": "TWO"
-        }
-      ]
-    },
-    {
-      "date": "20260529",
-      "label": "2026-05-29",
-      "has_market": true,
-      "stocks": [
-        {
-          "code": "2344",
-          "name": "華邦電",
-          "mkt": "TW"
-        },
-        {
-          "code": "3037",
-          "name": "欣興",
-          "mkt": "TW"
-        },
-        {
-          "code": "3105",
-          "name": "穩懋",
-          "mkt": "TWO"
-        },
-        {
-          "code": "6257",
-          "name": "矽格",
-          "mkt": "TW"
-        },
-        {
-          "code": "6261",
-          "name": "久元",
-          "mkt": "TWO"
-        }
-      ]
-    },
-    {
-      "date": "20260528",
-      "label": "2026-05-28",
-      "has_market": true,
-      "stocks": [
-        {
-          "code": "2344",
-          "name": "華邦電",
-          "mkt": "TW"
-        },
-        {
-          "code": "3037",
-          "name": "欣興",
-          "mkt": "TW"
-        },
-        {
-          "code": "3105",
-          "name": "穩懋",
-          "mkt": "TWO"
-        },
-        {
-          "code": "6257",
-          "name": "矽格",
-          "mkt": "TW"
-        },
-        {
-          "code": "6261",
-          "name": "久元",
-          "mkt": "TWO"
-        }
-      ]
-    },
-    {
-      "date": "20260527",
-      "label": "2026-05-27",
-      "has_market": true,
-      "stocks": [
-        {
-          "code": "2344",
-          "name": "華邦電",
-          "mkt": "TW"
-        },
-        {
-          "code": "3037",
-          "name": "欣興",
-          "mkt": "TW"
-        },
-        {
-          "code": "3105",
-          "name": "穩懋",
-          "mkt": "TWO"
-        },
-        {
-          "code": "6257",
-          "name": "矽格",
-          "mkt": "TW"
-        },
-        {
-          "code": "6261",
-          "name": "久元",
-          "mkt": "TWO"
         }
       ]
     }
