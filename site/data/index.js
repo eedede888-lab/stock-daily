81fa85bcd635f4c2e1f92c975b621d62b41e7b20
tongwade <18734529+tongwade@users.noreply.github.com>
2026-06-03 11:39:00 +0800

    更新個股資料：新增 20260527、20260528 並重建索引

--- site/data/index.js
+++ site/data/index.js
@@ -80,6 +80,60 @@ window.__DATAREG&&window.__DATAREG("index",{
           "name": "久元"
         }
       ]
+    },
+    {
+      "date": "20260528",
+      "label": "2026-05-28",
+      "has_market": true,
+      "stocks": [
+        {
+          "code": "2344",
+          "name": "華邦電"
+        },
+        {
+          "code": "3037",
+          "name": "欣興"
+        },
+        {
+          "code": "3105",
+          "name": "穩懋"
+        },
+        {
+          "code": "6257",
+          "name": "矽格"
+        },
+        {
+          "code": "6261",
+          "name": "久元"
+        }
+      ]
+    },
+    {
+      "date": "20260527",
+      "label": "2026-05-27",
+      "has_market": true,
+      "stocks": [
+        {
+          "code": "2344",
+          "name": "華邦電"
+        },
+        {
+          "code": "3037",
+          "name": "欣興"
+        },
+        {
+          "code": "3105",
+          "name": "穩懋"
+        },
+        {
+          "code": "6257",
+          "name": "矽格"
+        },
+        {
+          "code": "6261",
+          "name": "久元"
+        }
+      ]
     }
   ]
 });
