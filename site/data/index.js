626f18b86b538914ed5ef36c086350ada16704ee
eedede888-lab <eedede888@gmail.com>
2026-06-04 19:30:19 +0800

    更新 2026-06-03 盤後資料

--- site/data/index.js
+++ site/data/index.js
@@ -1,6 +1,38 @@
 window.__DATAREG&&window.__DATAREG("index",{
   "dates": [
     {
+      "date": "20260604",
+      "label": "2026-06-04",
+      "has_market": false,
+      "stocks": [
+        {
+          "code": "2344",
+          "name": "華邦電",
+          "mkt": "TW"
+        },
+        {
+          "code": "3037",
+          "name": "欣興",
+          "mkt": "TW"
+        },
+        {
+          "code": "3105",
+          "name": "穩懋",
+          "mkt": "TW"
+        },
+        {
+          "code": "6257",
+          "name": "矽格",
+          "mkt": "TW"
+        },
+        {
+          "code": "6261",
+          "name": "久元",
+          "mkt": "TW"
+        }
+      ]
+    },
+    {
       "date": "20260603",
       "label": "2026-06-03",
       "has_market": true,
