7ff4299740fda10ec3ba1592a832c6065183bc60
eedede888-lab <eedede888@gmail.com>
2026-06-05 12:44:22 +0800

    add new index.js with weekly_dates

---
+++ site/data/index.js
@@ -0,0 +1,103 @@
+window.__DATAREG&&window.__DATAREG("index",{
+  "dates": [
+    {
+      "date": "20260604",
+      "label": "2026-06-04",
+      "has_market": true,
+      "stocks": [
+        {
+          "code": "2344",
+          "name": "華邦電",
+          "mkt": "TW"
+        },
+        {
+          "code": "3037",
+          "name": "欣興",
+          "mkt": "TW"
+        },
+        {
+          "code": "3105",
+          "name": "穩懋",
+          "mkt": "TW"
+        },
+        {
+          "code": "6257",
+          "name": "矽格",
+          "mkt": "TW"
+        },
+        {
+          "code": "6261",
+          "name": "久元",
+          "mkt": "TW"
+        }
+      ]
+    },
+    {
+      "date": "20260603",
+      "label": "2026-06-03",
+      "has_market": true,
+      "stocks": [
+        {
+          "code": "2344",
+          "name": "華邦電",
+          "mkt": "TW"
+        },
+        {
+          "code": "3037",
+          "name": "欣興",
+          "mkt": "TW"
+        },
+        {
+          "code": "3105",
+          "name": "穩懋",
+          "mkt": "TW"
+        },
+        {
+          "code": "6257",
+          "name": "矽格",
+          "mkt": "TW"
+        },
+        {
+          "code": "6261",
+          "name": "久元",
+          "mkt": "TW"
+        }
+      ]
+    }
+  ],
+  "weekly_dates": [
+    {
+      "wkey": "20260529-0604",
+      "label": "2026-05-29 ~ 2026-06-04",
+      "start": "20260529",
+      "end": "20260604",
+      "stocks": [
+        {
+          "code": "2344",
+          "name": "華邦電",
+          "mkt": "TW"
+        },
+        {
+          "code": "3037",
+          "name": "欣興",
+          "mkt": "TW"
+        },
+        {
+          "code": "3105",
+          "name": "穩懋",
+          "mkt": "TW"
+        },
+        {
+          "code": "6257",
+          "name": "矽格",
+          "mkt": "TW"
+        },
+        {
+          "code": "6261",
+          "name": "久元",
+          "mkt": "TW"
+        }
+      ]
+    }
+  ]
+});
