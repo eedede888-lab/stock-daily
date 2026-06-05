6b6760b163c8a2d4003d5debc9727998d0df62bf
eedede888-lab <eedede888@gmail.com>
2026-06-05 11:42:38 +0800

    新增週報 20260529-0604

--- site/data/index.js
+++ site/data/index.js
@@ -65,5 +65,39 @@ window.__DATAREG&&window.__DATAREG("index",{
       ]
     }
   ],
-  "weekly_dates": []
+  "weekly_dates": [
+    {
+      "wkey": "20262529-0604",
+      "label": "2026-25-29 ~ 2026-06-04",
+      "start": "20262529",
+      "end": "20260604",
+      "stocks": [
+        {
+          "code": "2344",
+          "name": "華邦電",
+          "mkt": "TW"
+        },
+        {
+          "code": "3037",
+          "name": "欣興",
+          "mkt": "TW"
+        },
+        {
+          "code": "3105",
+          "name": "穩懋",
+          "mkt": "TW"
+        },
+        {
+          "code": "6257",
+          "name": "矽格",
+          "mkt": "TW"
+        },
+        {
+          "code": "6261",
+          "name": "久元",
+          "mkt": "TW"
+        }
+      ]
+    }
+  ]
 });
