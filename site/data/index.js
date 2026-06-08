09e92e81dd66adea368cefabd116d82da884697d
eedede888-lab <eedede888@gmail.com>
2026-06-08 17:27:05 +0800

    新增 20260608 資料

--- site/data/index.js
+++ site/data/index.js
@@ -1,8 +1,8 @@
 window.__DATAREG&&window.__DATAREG("index",{
   "dates": [
     {
-      "date": "20260605",
-      "label": "2026-06-05",
+      "date": "20260608",
+      "label": "2026-06-08",
       "has_market": true,
       "stocks": [
         {
@@ -31,11 +31,32 @@ window.__DATAREG&&window.__DATAREG("index",{
           "mkt": "TW"
         }
       ]
+    },
+    {
+      "date": "20260605",
+      "label": "2026-06-05",
+      "has_market": false,
+      "stocks": []
     },
     {
       "date": "20260604",
       "label": "2026-06-04",
-      "has_market": true,
+      "has_market": false,
+      "stocks": []
+    },
+    {
+      "date": "20260603",
+      "label": "2026-06-03",
+      "has_market": false,
+      "stocks": []
+    }
+  ],
+  "weekly_dates": [
+    {
+      "wkey": "20260605-0608",
+      "label": "2026-06-05 ~ 2026-06-08",
+      "start": "20260605",
+      "end": "20260608",
       "stocks": [
         {
           "code": "2344",
@@ -64,72 +85,12 @@ window.__DATAREG&&window.__DATAREG("index",{
         }
       ]
     },
-    {
-      "date": "20260603",
-      "label": "2026-06-03",
-      "has_market": true,
-      "stocks": [
-        {
-          "code": "2344",
-          "name": "華邦電",
-          "mkt": "TW"
-        },
-        {
-          "code": "3037",
-          "name": "欣興",
-          "mkt": "TW"
-        },
-        {
-          "code": "3105",
-          "name": "穩懋",
-          "mkt": "TW"
-        },
-        {
-          "code": "6257",
-          "name": "矽格",
-          "mkt": "TW"
-        },
-        {
-          "code": "6261",
-          "name": "久元",
-          "mkt": "TW"
-        }
-      ]
-    }
-  ],
-  "weekly_dates": [
     {
       "wkey": "20260529-0604",
       "label": "2026-05-29 ~ 2026-06-04",
       "start": "20260529",
       "end": "20260604",
-      "stocks": [
-        {
-          "code": "2344",
-          "name": "華邦電",
-          "mkt": "TW"
-        },
-        {
-          "code": "3037",
-          "name": "欣興",
-          "mkt": "TW"
-        },
-        {
-          "code": "3105",
-          "name": "穩懋",
-          "mkt": "TW"
-        },
-        {
-          "code": "6257",
-          "name": "矽格",
-          "mkt": "TW"
-        },
-        {
-          "code": "6261",
-          "name": "久元",
-          "mkt": "TW"
-        }
-      ]
+      "stocks": []
     }
   ]
 });
