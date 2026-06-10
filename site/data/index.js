ab71e9742fd31843c88e3ae5983144b8b87e6ab7
eedede888-lab <eedede888@gmail.com>
2026-06-10 19:29:23 +0800

    fix gitignore patchright

--- site/data/index.js
+++ site/data/index.js
@@ -1,6 +1,38 @@
 window.__DATAREG&&window.__DATAREG("index",{
   "dates": [
     {
+      "date": "20260610",
+      "label": "2026-06-10",
+      "has_market": true,
+      "stocks": [
+        {
+          "code": "2344",
+          "name": "華邦電",
+          "mkt": "TW"
+        },
+        {
+          "code": "3037",
+          "name": "欣興",
+          "mkt": "TW"
+        },
+        {
+          "code": "3105",
+          "name": "穩懋",
+          "mkt": "TWO"
+        },
+        {
+          "code": "6257",
+          "name": "矽格",
+          "mkt": "TW"
+        },
+        {
+          "code": "6261",
+          "name": "久元",
+          "mkt": "TW"
+        }
+      ]
+    },
+    {
       "date": "20260609",
       "label": "2026-06-09",
       "has_market": true,
@@ -18,7 +50,7 @@ window.__DATAREG&&window.__DATAREG("index",{
         {
           "code": "3105",
           "name": "穩懋",
-          "mkt": "TW"
+          "mkt": "TWO"
         },
         {
           "code": "6257",
@@ -50,7 +82,7 @@ window.__DATAREG&&window.__DATAREG("index",{
         {
           "code": "3105",
           "name": "穩懋",
-          "mkt": "TW"
+          "mkt": "TWO"
         },
         {
           "code": "6257",
