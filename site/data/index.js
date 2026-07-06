748b62de8cec8cab593b41d4ef8fc4cd271e3d20
eedede888-lab <eedede888@gmail.com>
2026-07-06 21:09:17 +0800

    新增 20260706 資料

--- site/data/index.js
+++ site/data/index.js
@@ -1,6 +1,63 @@
 window.__DATAREG&&window.__DATAREG("index",{
   "dates": [
     {
+      "date": "20260706",
+      "label": "2026-07-06",
+      "has_market": true,
+      "stocks": [
+        {
+          "code": "2313",
+          "name": "華通",
+          "mkt": "TW"
+        },
+        {
+          "code": "2317",
+          "name": "鴻海",
+          "mkt": "TW"
+        },
+        {
+          "code": "2330",
+          "name": "台積電",
+          "mkt": "TW"
+        },
+        {
+          "code": "2344",
+          "name": "華邦電",
+          "mkt": "TW"
+        },
+        {
+          "code": "3037",
+          "name": "欣興",
+          "mkt": "TW"
+        },
+        {
+          "code": "3105",
+          "name": "穩懋",
+          "mkt": "TWO"
+        },
+        {
+          "code": "6239",
+          "name": "力成",
+          "mkt": "TW"
+        },
+        {
+          "code": "6257",
+          "name": "矽格",
+          "mkt": "TW"
+        },
+        {
+          "code": "6261",
+          "name": "久元",
+          "mkt": "TWO"
+        },
+        {
+          "code": "8027",
+          "name": "鈦昇",
+          "mkt": "TWO"
+        }
+      ]
+    },
+    {
       "date": "20260703",
       "label": "2026-07-03",
       "has_market": true,
