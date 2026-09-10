e1343bdab4ca599241b10b9ae5f1428f986c21f7
eedede888-lab <eedede888@gmail.com>
2026-09-10 20:27:46 +0800

    新增 20260910 & 20260904-0910資料

--- site/data/index.js
+++ site/data/index.js
@@ -1,6 +1,63 @@
 window.__DATAREG&&window.__DATAREG("index",{
   "dates": [
     {
+      "date": "20260910",
+      "label": "2026-09-10",
+      "has_market": true,
+      "stocks": [
+        {
+          "code": "0069",
+          "name": "",
+          "mkt": "TW"
+        },
+        {
+          "code": "2313",
+          "name": "華通",
+          "mkt": "TW"
+        },
+        {
+          "code": "2330",
+          "name": "台積電",
+          "mkt": "TW"
+        },
+        {
+          "code": "2344",
+          "name": "華邦電",
+          "mkt": "TW"
+        },
+        {
+          "code": "3037",
+          "name": "欣興",
+          "mkt": "TW"
+        },
+        {
+          "code": "3105",
+          "name": "穩懋",
+          "mkt": "TWO"
+        },
+        {
+          "code": "6239",
+          "name": "力成",
+          "mkt": "TW"
+        },
+        {
+          "code": "6257",
+          "name": "矽格",
+          "mkt": "TW"
+        },
+        {
+          "code": "6261",
+          "name": "久元",
+          "mkt": "TWO"
+        },
+        {
+          "code": "8027",
+          "name": "鈦昇",
+          "mkt": "TWO"
+        }
+      ]
+    },
+    {
       "date": "20260909",
       "label": "2026-09-09",
       "has_market": true,
@@ -3522,6 +3579,59 @@ window.__DATAREG&&window.__DATAREG("index",{
     }
   ],
   "weekly_dates": [
+    {
+      "wkey": "20260904-0910",
+      "label": "2026-09-04 ~ 2026-09-10",
+      "start": "20260904",
+      "end": "20260910",
+      "stocks": [
+        {
+          "code": "0069",
+          "name": "",
+          "mkt": "TW"
+        },
+        {
+          "code": "2313",
+          "name": "華通",
+          "mkt": "TW"
+        },
+        {
+          "code": "2344",
+          "name": "華邦電",
+          "mkt": "TW"
+        },
+        {
+          "code": "3037",
+          "name": "欣興",
+          "mkt": "TW"
+        },
+        {
+          "code": "3105",
+          "name": "穩懋",
+          "mkt": "TWO"
+        },
+        {
+          "code": "6239",
+          "name": "力成",
+          "mkt": "TW"
+        },
+        {
+          "code": "6257",
+          "name": "矽格",
+          "mkt": "TW"
+        },
+        {
+          "code": "6261",
+          "name": "久元",
+          "mkt": "TWO"
+        },
+        {
+          "code": "8027",
+          "name": "鈦昇",
+          "mkt": "TWO"
+        }
+      ]
+    },
     {
       "wkey": "20260828-0903",
       "label": "2026-08-28 ~ 2026-09-03",
